1a1e6124fcacfbe5d3bdf0bac5bbe744c112f57f
TUSHAR <70511629+Tushar96152@users.noreply.github.com>
2025-04-14 18:10:17 +0530

    Update UserController.java

--- src/main/java/com/Banking/backend/controller/UserController.java
+++ src/main/java/com/Banking/backend/controller/UserController.java
@@ -4,6 +4,7 @@ package com.Banking.backend.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
